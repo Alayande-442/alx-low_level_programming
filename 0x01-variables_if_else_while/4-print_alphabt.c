d45d550da4b92adf061f3cdb8c39259a0000f76c
Alayande-442 <alayandesteven@gmail.com>
2023-07-16 13:12:20 -0700

    lower

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio .h>
+
+/**
+ * main - print alphabet in lower excluding q and e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+        char lw;
+
+        for (lw = 'a' lw <= 'z' lw++)
+        {
+                if (lw != 'q' && lw != 'e')
+			putchar(lw);
+        }
+	putchar('\n');
+	return (0);
+}
