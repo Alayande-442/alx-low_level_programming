9de8bbdb702699e3862dc22a1b0973c03ee08e3f
Alayande-442 <alayandesteven@gmail.com>
2023-07-16 12:03:51 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - write a code to print alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar("%c ", alphabet);
+	}
+	putchar("\n");
+	return 0;
+}
