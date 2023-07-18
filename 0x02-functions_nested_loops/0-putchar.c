6053d2c96153ec905315875187228f146225e4a7
Alayande-442 <alayandesteven@gmail.com>
2023-07-18 14:26:51 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+
+	return (0);
+}
