ce7daefe06db30834002942335590b9117638d28
Alayande-442 <alayandesteven@gmail.com>
2023-07-18 16:31:21 -0700

    lower case letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - write a code that print a lowercase alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int lw;
+	for (lw = 'a'; lw <= 'z'; lw++)
+	{
+		_putchar(lw);
+		
+		_putchar('\n');
+		return (0);
+	}
+}
