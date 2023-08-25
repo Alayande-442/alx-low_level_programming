b23ee7d484ff3670950174bcc27285b9de684e2e
Alayande-442 <alayandesteven@gmail.com>
2023-08-25 14:40:28 -0700

    task-100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - prints  sentence before the main function
+ * function that is executed
  */
 void first(void)
 {
