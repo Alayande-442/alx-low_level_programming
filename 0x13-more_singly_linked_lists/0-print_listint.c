7bdd2ee941419ec136ef93689e0c82b3513a2493
Alayande-442 <alayandesteven@gmail.com>
2023-08-29 10:53:15 -0700

    task-00

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t w = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		w++;
 		h = h->next;
 	}
 
-	return (num);
+	return (w);
 }
