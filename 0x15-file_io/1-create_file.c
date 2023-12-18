f5c482a3170cf01e2a3854e8fe26b02b050b2c95
Alayande-442 <alayandesteven@gmail.com>
2023-12-18 05:55:06 -0800

    task-1 create a file and write input

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - we are to create a file and input text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: we name the file create_file
+ * @text_content: the text that will be written inside the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
+	}
+
+	if (close(file) == -1 || inlen != length)
+		return (-1);
+	return (1);
+}
